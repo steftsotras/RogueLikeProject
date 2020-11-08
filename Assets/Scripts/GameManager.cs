2c65bf65e53d6dcfc629611729e36b65ce5f9d6d
Stefanos Tsotras <steftsotras2@gmail.com>
2020-11-08 03:08:16 +0200

    init

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    // Making GameManager a Singleton
+    public static GameManager instance = null;
+
+    public BoardManager boardScript;
+    
+    private int level = 3;
+
+    void Awake() {
+        if(instance == null){
+            instance = this;
+        }
+        else{
+            Destroy(gameObject);
+        }
+
+        //Persist between scenes
+        DontDestroyOnLoad(gameObject);
+
+        boardScript = GetComponent<BoardManager>();
+        InitGame();    
+    }
+
+    void InitGame(){
+        boardScript.SetupScene(level);
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    //[Header("Input settings:")]
+    [Space]
+    [Header("Character attributes:")]
+    public float MOVEMENT_BASE_SPEED = 1.0f;
+
+    [Space]
+    [Header("Character Statistics:")]
+    public Vector2 movementDirection;
+    public float movementSpeed;
+
+    [Space]
+    [Header("References:")]
+    public Rigidbody2D rb;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInputs();
+        Move();
+    }
+
+    void ProcessInputs(){
+        movementDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        movementSpeed = Mathf.Clamp(movementDirection.magnitude, 0.0f, 1.0f);
+        movementDirection.Normalize();
+    }
+
+    void Move(){
+        rb.velocity = movementDirection * movementSpeed * MOVEMENT_BASE_SPEED;
+    }
+}
