da6420bc4f147ebae9ccdf0da8ffb5260e2cf5d5
aggelospot <aggelospot@users.noreply.github.com>
2020-11-09 06:59:49 +0200

    Add files via upload

---
+++ Animations + Movement/Scripts/Movement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour{
+    public Animator animator;
+
+    // Update is called once per frame
+    void Update(){
+
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0.0f);
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Magnitude", movement.magnitude);
+
+        transform.position = transform.position + movement * Time.deltaTime;
+    }
+}
